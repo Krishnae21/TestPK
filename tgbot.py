6609cb0ea00c2237bb14184ac1dc6b303f0a1296
vavan <vasiliew.vov1998@gmail.com>
2023-10-09 17:30:17 +0300

    done

--- tgbot.py
+++ tgbot.py
@@ -4,25 +4,24 @@ import redis
 import telebot
 
 bot = telebot.TeleBot("6612234170:AAGqKFvH0G5kyo9dVPtCmopBow9Pj_QrFo0")
-r = redis.Redis(host='localhost', port=5370, db=0)
+r = redis.from_url("redis://redis:5470")
 
 
 @bot.message_handler(content_types=["text"])
 def get_factorial(message):
     try:
         msg: int = int(message.text)
-        fact = r.get(str(msg)).decode()
+        fact = r.get(str(msg))
         if fact is None:
             fact = math.factorial(msg)
             if msg >= 1000 or msg <= -1000:
                 fact = str(fact)[0:5]
             r.set(str(msg), fact)
-
-
-
-
-
+        else:
+            fact.decode()
+        bot.send_message(message.from_user.id, fact)
     except ValueError as ex:
+        print(ex)
         bot.send_message(
             message.from_user.id, "Введите число для получения факториала."
         )
